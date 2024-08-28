51223d516dd025dfe449799b112407319da7440c
suzuka <kh.126@icloud.com>
2024-08-28 20:53:31 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: 'ユーザー作成に成功しました'
+      redirect_to root_path, notice: "ユーザー作成に成功しました"
     else
       flash.now[:alert] = "ユーザー作成に失敗しました"
       render :new, status: :unprocessable_entity
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # user
   resources :users, only: %i[new create]
   # login
-  get 'login' => 'user_sessions#new', :as => :login
-  post 'login' => "user_sessions#create"
-  delete 'logout' => 'user_sessions#destroy', :as => :logout
+  get "login" => "user_sessions#new", :as => :login
+  post "login" => "user_sessions#create"
+  delete "logout" => "user_sessions#destroy", :as => :logout
 end
