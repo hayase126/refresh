d2e4f10cbf77e7dbd60c946277577eaa228e7495
suzuka <kh.126@icloud.com>
2024-08-31 21:26:35 +0900

    修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,15 +12,15 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to posts_path, success: '投稿に成功しました'
+      redirect_to posts_path, success: "投稿に成功しました"
     else
-      flash.now[:alert] = '投稿に失敗しました'
+      flash.now[:alert] = "投稿に失敗しました"
       render :new, status: :unprocessable_entity
     end
   end
 
   private
   def post_params
-    params.require(:post).permit(:title, :content)#, :post_image, :post_image_cache)
+    params.require(:post).permit(:title, :content) #, :post_image, :post_image_cache)
   end
 end
