a8c97539be188c44e40e4a328d5aae4beac4b40b
suzuka <kh.126@icloud.com>
2024-09-02 20:01:46 +0900

    schema.rb編集

--- db/schema.rb
+++ db/schema.rb
@@ -25,11 +25,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_30_013308) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", null: false
     t.string "crypted_password"
     t.string "salt"
-    t.string "profile_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
