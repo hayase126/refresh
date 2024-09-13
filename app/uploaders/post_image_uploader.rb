b63d9142dd98c6a7d5e3bf920e858cc481f56ddf
suzuka <kh.126@icloud.com>
2024-09-13 23:01:40 +0900

    carriewave.rb変更

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -26,7 +26,7 @@ class PostImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [1000, 1000]
+  process resize_to_fill: [ 1000, 1000 ]
   #
   # def scale(width, height)
   #   # do something
