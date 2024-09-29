7512ce0782d568e03d0f6e9d1bda02821bd9391c
suzuka <kh.126@icloud.com>
2024-09-30 04:28:44 +0900

    修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,14 @@ require "carrierwave/storage/fog"
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
-    config.fog_directory  = myapp-refresh
+    config.fog_provider = "fog/aws"
+    config.fog_directory  = "myapp-refresh"
     config.fog_public = false
     config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: ap-northeast-1,
+      region: "ap-northeast-1",
       path_style: true
     }
   else
