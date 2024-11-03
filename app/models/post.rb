d2e7fa1b20f79e93b1bc1c5c6757e037085dfeed
suzuka <kh.126@icloud.com>
2024-11-03 22:16:53 +0900

    タグの表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,25 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: { maximum: 65_535 }
 
+  scope :with_tag, ->(tag_name) { joins(:tags).where(tags: { name: tag_name }) }
+
   def post_image_present?
     post_image.present?
+  end
+
+  def save_with_tags(tag_names:)
+    ActiveRecord::Base.transaction do
+      self.tags = tag_names.map { |name| Tag.find_or_initialize_by(name: name.strip) }
+      save!
+    end
+    true
+  rescue StandardError
+    false
+  end
+
+  # 編集時デフォルトで表示するためのメソッド
+  def tag_names
+    # NOTE: pluckだと新規作成失敗時に値が残らない(返り値がnilになる)
+    tags.map(&:name).join(',')
   end
 end
