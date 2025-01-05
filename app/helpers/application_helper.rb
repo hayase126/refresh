0669f69b5fb4eee4f9bde32f8395cf50f3b6715a
suzuka <kh.126@icloud.com>
2025-01-05 21:32:19 +0900

    OGP表示・favicon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,30 @@ module ApplicationHelper
     else 'bg-yellow-400'
     end
   end
+
+  def default_meta_tags
+    {
+      site: 'りふれっ酒',
+      title: 'りふれっ酒',
+      reverse: true,
+      charset: 'utf-8',
+      description: '二日酔いで困ってませんか？',
+      keywords: '二日酔い,酒,気持ち悪い',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('favicon.png') },
+        { href: image_url('favicon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' }
+      ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('favicon.png'),
+        locale: 'ja-JP'
+      }
+    }
+  end
 end
