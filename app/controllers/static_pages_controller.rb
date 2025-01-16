61e3ca5ed12f903f30d9423080a8d20deb3081cd
suzuka <kh.126@icloud.com>
2025-01-16 21:26:30 +0900

    プライバシーポリシー作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[top form]
+  skip_before_action :require_login, only: %i[top form policy]
 
   def top; end
 
   def form; end
+
+  def policy; end
+  
 end
